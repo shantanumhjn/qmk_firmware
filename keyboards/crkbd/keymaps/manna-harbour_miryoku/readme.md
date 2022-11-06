8c1d07278c98146857b6358d403033e09bcb1c8e
Shantanu Mahajan <shantanumhjn@gmail.com>
2022-11-06 20:10:22 +0530

    disable backlight, enable oled, balloon animation

---
+++ keyboards/crkbd/keymaps/manna-harbour_miryoku/readme.md
@@ -0,0 +1,2 @@
+build command:
+qmk compile -c -kb crkbd -km manna-harbour_miryoku -e MIRYOKU_ALPHAS=COLEMAK -e MIRYOKU_EXTRA=QWERTY
--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,7 +1,7 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
-RGB_MATRIX_ENABLE 	= yes
+RGB_MATRIX_ENABLE 	= no
 OLED_ENABLE     	= yes
 OLED_DRIVER     	= SSD1306
 LTO_ENABLE      	= yes
