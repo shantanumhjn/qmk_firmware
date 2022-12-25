ee5dd8b6dbfad194d814ce8ee3d4b988c65d06c0
Shantanu Mahajan <shantanu.mahajan@oracle.com>
2022-12-25 15:17:11 +0530

    create crkbd specific rules.mk

---
+++ keyboards/crkbd/keymaps/manna-harbour_miryoku/rules.mk
@@ -0,0 +1,11 @@
+# Copyright 2022 Shantanu Mahajan
+
+RGB_MATRIX_ENABLE 	= no
+OLED_ENABLE     	= yes
+OLED_DRIVER     	= SSD1306
+LTO_ENABLE      	= yes
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+
+BOOTLOADER 			= atmel-dfu
